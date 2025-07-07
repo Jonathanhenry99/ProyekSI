e4c77290d3497330cc98ed91f38152d287086d05
HayyaVevilaFaisal <hayyavevilaa@gmail.com>
2025-07-07 18:01:11 +0700

    menambahkan fitur indikator kelengkapan soal

--- bank-soal-backend/routes/file.routes.js
+++ bank-soal-backend/routes/file.routes.js
@@ -36,4 +36,7 @@ module.exports = function(app) {
 
   // Route untuk menggabungkan file dari satu question set
   app.get("/api/files/combine-preview/:id", controller.combineFilesForPreview);
+
+  // Indikator kelengkapan soal berdasarkan questionSetId (publik)
+  app.get("/api/files/completeness/:questionSetId", controller.getFileCompleteness);
 };
