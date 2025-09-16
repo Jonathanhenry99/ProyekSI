c9fd323f406463489068bef31ab1fce119621ec5
Clarence <ferdinandclarence5@gmail.com>
2025-09-16 07:07:12 +0700

    Merge admin dan main ke dalam branch clarence

--- bank-soal-backend/config/db.config.js
+++ bank-soal-backend/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     HOST: "localhost",
     USER: "postgres",     // Change this to your PostgreSQL username
-    PASSWORD: "Juliadi1945", // Change this to your PostgreSQL password
-    DB: "bank_soal_db",
+    PASSWORD: "postgres", // Change this to your PostgreSQL password
+    DB: "ProyekSI",
     PORT: 5432,
     dialect: "postgres",
     pool: {
